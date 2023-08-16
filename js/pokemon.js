8f43d8e1c8132fffb14ebb638d93295010dd484f
jeheald23 <jeheald23@gmail.com>
2023-08-16 15:53:07 +0200

    Create pokemon.js

---
+++ js/pokemon.js
@@ -0,0 +1,15 @@
+let pokemonList = [{
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['grass', 'poison']
+},
+{
+    name: 'Charizard',
+    height: 1.7,
+    type: ['fire', 'flying']
+},
+{
+    name: 'Jigglypuff',
+    height: 0.5,
+    type: ['fairy', 'normal']
+}];
