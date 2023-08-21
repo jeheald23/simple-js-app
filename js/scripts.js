43350832ef284d9685166fe134ed92d2e407cb2e
jeheald23 <jeheald23@gmail.com>
2023-08-21 14:46:39 +0200

    replaced alert with arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
-
-alert("Hello world!");
-
-favoriteFood = "pizza";
-document.write(favoriteFood);
-
+let pokemonList = [{
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['grass', 'poison']
+},
+{
+    name: 'Charizard',
+    height: 1.7,
+    type: ['fire', 'flying']
+},
+{
+    name: 'Jigglypuff',
+    height: 0.5,
+    type: ['fairy', 'normal']
+}];
