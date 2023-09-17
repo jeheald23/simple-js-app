9f2a724e0c72674b581ee659416222725cc51ef0
jeheald23 <jeheald23@gmail.com>
2023-09-17 18:09:56 +0200

    wrapped code in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,38 @@
-let pokemonList = [{
-    name: 'Bulbasaur',
-    height: 0.7,
-    type: ['grass', 'poison']
-},
-{
-    name: 'Charizard',
-    height: 1.7,
-    type: ['fire', 'flying']
-},
-{
-    name: 'Jigglypuff',
-    height: 0.5,
-    type: ['fairy', 'normal']
-}];
+let pokemonRepository = (function () {
+    
+    let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        type: ['grass', 'poison']
+    },
 
-pokemonList.forEach(function(item){
-    document.write("<p>" + "Name: " + item.name + " Height: " + item.height + "m" + " Type: " + item.type);
-});
+    {
+        name: 'Charizard',
+        height: 1.7,
+        type: ['fire', 'flying']
+    },
+
+    {
+        name: 'Jigglypuff',
+        height: 0.5,
+        type: ['fairy', 'normal']
+    }
+    ]; 
+    
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+      }
+    
+      function getAll() {
+        return pokemonList;
+      }
+    
+      return {
+        add: add,
+        getAll: getAll
+      };
+    })();
+
+  console.log(pokemonRepository.getAll())
+
