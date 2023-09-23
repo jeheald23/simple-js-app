ec4ee2f28d621611b1ff150b68f416140014dc18
jeheald23 <jeheald23@gmail.com>
2023-09-23 18:57:05 +0200

    added comments to code, created a button and event listening and handling for button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
+
+//IIFE to keep this code from being accessed externally
+
 let pokemonRepository = (function () {
     
-    let pokemonList = [
+  //creates an array of pokemon objects
+    
+  let pokemonList = [
     {
         name: 'Bulbasaur',
         height: 0.7,
@@ -17,24 +22,61 @@ let pokemonRepository = (function () {
         name: 'Jigglypuff',
         height: 0.5,
         type: ['fairy', 'normal']
-    }
+    },
+
+    {
+      name: 'Charmander',
+      height: 0.6,
+      types: ['fire']
+  }
     ]; 
     
+    // function to add pokemon
     function add(pokemon) {
         pokemonList.push(pokemon);
       }
     
+      //returns pokemon array
       function getAll() {
         return pokemonList;
       }
-    
+
+    //creates pokemon buttons
+      
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('button-class');
+        listItem.appendChild(button);
+        pokemonList.appendChild(listItem);
+        
+        //listens for a click and calls the function to console log pokemon
+        
+        button.addEventListener('click', function (event) {
+        showDetails(pokemon);
+        });
+      }
+
+      //function that console logs pokemon
+      
+      function showDetails(pokemon){
+        console.log(pokemon)
+      }
+
+      // returns objects 
+      
       return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem
       };
     })();
 
-    pokemonRepository.getAll().forEach(function(item){ 
-      document.write(`<p>Name: ${item.name} Height: ${item.height}m Type: ${item.type}`); 
-    });
+    //allows the code in IIFE to be called externally
+    
+    pokemonRepository.getAll().forEach(function(pokemon){ 
+      pokemonRepository.addListItem(pokemon)
 
+    });
